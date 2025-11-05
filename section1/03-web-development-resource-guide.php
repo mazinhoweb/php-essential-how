a72ae4efa0331cc784cd0779db2d850857b60c61
Mazinho Web <mazinhoweb@outlook.com>
2025-11-05 18:23:21 -0300

    Updating section1 - 03-web-development-resource

---
+++ section1/03-web-development-resource-guide.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Essential How - Section 1 - Web development Resource Guide</title><link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body { font-family: "Open Sans", sans-serif; font-optical-sizing: auto; font-style: normal; padding-top: 6px; /* Adjust for fixed navbar */}
+        .container { margin-bottom: 10px; } 
+        .jumbotron { padding: 2rem 1rem; margin-bottom: 0rem; background-color: #e9ecef; border-radius: .3rem; }
+        .section-spacing { padding-top: 10px; }
+        h2, h2 { font-size: 1.6rem; }
+        pre { background-color: #f8f9fa; padding: 15px; border-radius: 5px; overflow-x: auto; }
+        footer .container { margin-bottom: 0px; padding-bottom: 10px; font-size: 0.8rem; } 
+    </style>
+</head>
+<body>
+    <div class="container">
+
+        <section id="php-essential" class="jumbotron text-center">
+            <h1 class="display-5">PHP Essential How</h1>
+        </section>
+
+        <section id="github" class="section-spacing">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Section 1</h2>
+                </div>
+                <div class="card-header">
+                    <h4>Web Development Resource Guide 15 pages</h4>
+                </div>
+                <div class="card-body">
+                    <p class="lead"><pre>Web Development Resource Guide List: <a href="http://localhost/section1/03-Web-Development-Resource-Guide-List.pdf" target="_blank">03-Web-Development-Resource-Guide-List.pdf</a></pre></p>
+                </div>
+            </div>
+        </section>
+
+        <section id="description" class="section-spacing">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Description</h3>
+                </div>
+                <div class="card-body">
+                    <p><a href="http://localhost/section1/01-php-course-introduction.php" target="_blank">PHP Course Introduction 02:49</a><br>
+                        <a href="http://localhost/section1/02-php-resources-file.php" target="_blank">PHP Resources File 02:50</a><br>
+                        <a href="http://localhost/section1/03-web-development-resource-guide.php" target="_blank">Web development Resource Guide 15 pages</a><br>
+                        <a href="http://localhost/section1/04-php-setup-local-host-for-server-side-scripting.php" target="_blank">PHP setup local host for server side scripting 06:59</a><br>
+                        <a href="http://localhost/section1/05-php-setup-config-install.php" target="_blank">PHP setup config install 05:15</a></p>
+                </div>
+            </div>
+        </section>
+
+        <section id="docker-url" class="section-spacing">
+            <div class="card">
+                <div class="card-header">
+                    <h5>URL Address</h5>
+                </div>
+                <div class="card-body">
+                    <p><a href="http://localhost/section1/02-php-resources-file.php" target="_blank">http://localhost/section1/02-php-resources-file.php</a></p>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">PHP Essential How: Edmar Inocencio - Mazinho Developer - Mazinho Web.</span>
+        </div>
+    </footer>
+</body>
+</html>
